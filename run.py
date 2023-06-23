022e4321bbc40a4eac2d07642a99a6c12b3d9b9e
panalexeu <pan.alexeu@gmail.com>
2023-06-23 15:21:43 +0300

    minor refactor

--- run.py
+++ run.py
@@ -5,6 +5,7 @@ import constants
 import databse
 import info_commands
 import music_commands
+import queue_commands
 
 intents = discord.Intents.all()
 client = discord.Client(intents=intents)
@@ -14,6 +15,7 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 @bot.event
 async def on_ready():
     await bot.add_cog(music_commands.MusicCommands(bot))
+    await bot.add_cog(queue_commands.QueueCommands(bot))
     await bot.add_cog(info_commands.InfoCommands())
 
 if __name__ == '__main__':
